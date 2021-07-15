73164fcba637c181118889162654925d514e79ba
QuatschVirus <quatschvirus@gmail.com>
2021-07-15 21:49:31 +0200

    Added Connect4 (in dev)

---
+++ Connect4/README.md
@@ -0,0 +1,3 @@
+# Connect4
+A recreation of the Connect4 board game. There is nothing more to say, it should be selfexplanatory.
+## This is under development. Its unfinished, and I don't recommend downloading this.
---
+++ Connect4/main.py
@@ -0,0 +1,10 @@
+from ConsoleBooster.FancyText import Grid
+from osplus import Configs, Logs
+
+
+class Tile:
+    def __init__(self, x, y, letter):
+        self.x = x
+        self.y = y
+        self.letter = letter
+
